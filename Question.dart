d1548c3c209a0b15d53bdea38c0df6ef13330543
Jubayer Hasan Munna <185611999+vexlore-code@users.noreply.github.com>
2025-06-25 23:32:06 +0600

    Initial commit

--- Question.dart
+++ Question.dart
@@ -16,22 +16,21 @@ void main() {
 
   Map<String, double> averages = {};
   for (var student in students) {
-    String name = student['name'];
     List<int> scores = List<int>.from(student['scores']);
-    double avg = scores.reduce((a, b) => a + b) / scores.length;
-    averages[name] = double.parse(avg.toStringAsFixed(2));
+    double sum = 0;
+    for (var score in scores) {
+      sum += score;
+    }
+    double avg = sum / scores.length;
+    averages[student['name']] = double.parse(avg.toStringAsFixed(2));
   }
 
   var sortedEntries = averages.entries.toList()
     ..sort((a, b) => b.value.compareTo(a.value));
 
-  Map<String, double> sortedAverages = {
-    for (var entry in sortedEntries) entry.key: entry.value,
-  };
-
   print("{");
-  sortedAverages.forEach((key, value) {
-    print('  "$key": $value,');
-  });
+  for (var entry in sortedEntries) {
+    print('  "${entry.key}": ${entry.value},');
+  }
   print("}");
 }
